6a1f1046c6b58e0479fb475d0d3c948fdf67faad
Crowdbotics <team@crowdbotics.com>
2021-08-05 10:33:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "throbbing_block_29502Identifier",
+  url: "https://throbbing_block_29502Identifier.botics.co",
+  api: "https://throbbing_block_29502Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
